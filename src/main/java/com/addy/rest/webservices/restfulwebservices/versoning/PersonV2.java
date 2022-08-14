b2e137eb7dfd398fec38d692d827524342c3d7e1
chandan032 <chandan.adhikari08@gmail.com>
2022-08-14 21:20:32 +0530

    restfull api spring boot

---
+++ src/main/java/com/addy/rest/webservices/restfulwebservices/versoning/PersonV2.java
@@ -0,0 +1,28 @@
+package com.addy.rest.webservices.restfulwebservices.versoning;
+
+public class PersonV2 {
+
+	private Name name;
+	
+	
+	
+	public PersonV2() {
+		super();
+	}
+
+	public PersonV2(Name name) {
+		super();
+		this.name = name;
+	}
+
+	public Name getName() {
+		return name;
+	}
+
+	public void setName(Name name) {
+		this.name = name;
+	}
+
+	
+	
+}
---
+++ target/classes/data.sql
@@ -0,0 +1,5 @@
+insert into user values(10001, CURRENT_DATE(), 'AB'); 
+insert into user values(10002, CURRENT_DATE(), 'Jill'); 
+insert into user values(10003, CURRENT_DATE(), 'Jam'); 
+insert into post values(20001, 'My First Post', 10001);
+insert into post values(20002, 'My Second Post', 10001);
